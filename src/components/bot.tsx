1a25fc56e14eda3013756b26bb8d7f23c7c7542c
bayanKodehode <116548227+BayanKodehode@users.noreply.github.com>
2022-12-31 06:20:39 +0100

    better format

--- src/components/bot.tsx
+++ src/components/bot.tsx
@@ -23,39 +23,43 @@ export const Bot = ({ greeting }: IBot) => {
           <>
             {riddles[0].title}
             <br />
+            <br />
             {riddles[0].question}
             <br />
-            {/* {riddles[0].answer} */}
+            <br />
+            {riddles[0].answer}
           </>,
         ]);
         console.log(riddles);
       } else {
         // handle case where riddles array is empty
         // need to find a solution to this later
-        setConversation(["Sorry, but don't give up! Keep trying and maybe you'll get lucky."]);
+        setConversation([
+          "Sorry, but don't give up! Keep trying and maybe you'll get lucky.",
+        ]);
       }
     } catch (error) {
       console.log(error);
     }
   };
-  
 
   const onJokeRequest = async () => {
     try {
       await fetchJokes();
       if (jokes.length > 0) {
-        setConversation([ <>{jokes[0].joke}</>]);
+        setConversation([<>{jokes[0].joke}</>]);
         console.log(jokes);
       } else {
         // handle case where jokes array is empty
         // need to find a solution to this later
-        setConversation(["Don't worry though, just click again and maybe I'll have a joke for you next time!"]);
+        setConversation([
+          "Don't worry though, just click again and maybe I'll have a joke for you next time!",
+        ]);
       }
     } catch (error) {
       console.log(error);
     }
   };
-  
 
   const toggleVisibility = () => {
     setVisibility(!visibility);
